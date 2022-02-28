c3f678cf456081d39c6f9ba5128be2631c795d4d
Tom Stein <dev@tomstein.me>
2022-02-28 18:58:30 +0100

    Revert "gernerate python dependencies in venv"
    This reverts commit 1dd345431df8ccc533082d1d327e603add160081.

--- renovate-config/config.js
+++ renovate-config/config.js
@@ -1,11 +1,8 @@
 module.exports = {
   allowPostUpgradeCommandTemplating: true,
   allowedPostUpgradeCommands: [
-    "^python3 -m venv venv$",
-    "^source venv/bin/activate$",
-    "^source venv/bin/activate && cd project && pip3 install -r requirements.txt",
-    "^source venv/bin/activate && cd project && pip-licenses --ignore-packages pkg-resources --with-urls --format=csv --output-file=third-party-licenses/third-party-licenses.csv$",
-    "^source venv/bin/activate && cd project && pip-licenses --ignore-packages pkg-resources --summary --output-file=third-party-licenses/third-party-licenses-summary.txt$"
+    '^cd project && pip3 install',
+    '^cd project && pip-licenses'
   ],
 };
   
